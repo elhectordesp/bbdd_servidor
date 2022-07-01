0dc6a15ef14117098996b93378a313fcd3fbd638
Héctor <hectordura74@gmail.com>
2022-07-01 12:56:40 +0200

    subida

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT | 3999;
 
-mongoose.connect('mongodb://localhost:27017/tfg-hector', (err, res) => {
+// Importar variables de entorno locales
+require('dotenv').config({ path: 'variables.env'});
+console.log(process.env.DB_URL);
+
+mongoose.connect(process.env.DB_URL, (err, res) => {
     if (err) {
         throw err;
     } else {
         console.log('La conexion a la base de datos esta corriendo.');
 
-        app.listen(port, () => {
+        // Leer localhost de variables
+        const host = process.env.HOST || '0.0.0.0';
+        app.listen(port, host, () => {
             console.log('Servidor corriendo');
         })
     }
